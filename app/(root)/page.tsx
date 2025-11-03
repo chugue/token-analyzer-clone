a5d703796352f94a34a25a35b81cd5cd0ea21129
chugue <chugue85@gmail.com>
2025-11-04 00:27:42 +0900

    feat(ui): root page ui

---
+++ app/(root)/page.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  InputGroup,
+  InputGroupAddon,
+  InputGroupInput,
+} from "@/components/ui/input-group";
+import { Search } from "lucide-react";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen gap-6">
+      <div className="flex flex-col gap-2">
+        <h1 className="text-4xl font-bold">Token Social Analyzer</h1>
+        <div className="text-lg font-light">
+          AI-powered Twitter analysis for crypto tokens
+        </div>
+      </div>
+      <Card className="flex flex-col w-full max-w-md gap-1 shadow-lg">
+        <CardHeader className="text-start">
+          <CardTitle className="text-md font-medium">Token Symbol</CardTitle>
+        </CardHeader>
+        <CardContent className="flex flex-col gap-3">
+          <InputGroup>
+            <InputGroupInput placeholder="BTC, ETH, SOL.. " />
+            <InputGroupAddon>
+              <Search />
+            </InputGroupAddon>
+          </InputGroup>
+          <Button className="w-full" disabled={true}>
+            Generate Report
+          </Button>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
